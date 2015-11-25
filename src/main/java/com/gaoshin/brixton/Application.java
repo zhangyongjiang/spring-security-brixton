9450e43e8b9144efa0eff2c6672c67d4d54d11b4
Kevin Zhang <zhangyongjiang@gmail.com>
2015-11-24 17:13:38 -0800

    without EnableOAuth2Sso annotation, curl command returns
    {"timestamp":1448414014091,"status":401,"error":"Unauthorized","message":"Full authentication is required to access this resource","path":"/hi"}

--- src/main/java/com/gaoshin/brixton/Application.java
+++ src/main/java/com/gaoshin/brixton/Application.java
@@ -1,14 +1,25 @@
 package com.gaoshin.brixton;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@EnableOAuth2Sso
+//@EnableOAuth2Sso
+@EnableAutoConfiguration
 public class Application {
-	@RequestMapping("/")
+	@RequestMapping(name="/hi", method=RequestMethod.GET)
 	public String home() {
 		return "Hello World";
 	}
+	
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
